72fee69925bd4f81e8cc617d142aebb777f623c4
caahcavalcantte <caahcavalcante.alfa@gmail.com>
2024-07-28 15:01:09 -0300

    configurando o menu hamburguer para fechar ao clicar no link

--- script.js
+++ script.js
@@ -3,9 +3,18 @@
 document.addEventListener('DOMContentLoaded', function() {
   const menuToggle = document.getElementById('menu-toggle');
   const menuNav = document.getElementById('menu-nav');
+  const menuLinks = menuNav.querySelectorAll('a'); 
 
+  
   menuToggle.addEventListener('click', function() {
-    menuNav.classList.toggle('active');
+      menuNav.classList.toggle('active');
+  });
+
+
+  menuLinks.forEach(function(link) {
+      link.addEventListener('click', function() {
+          menuNav.classList.remove('active');
+      });
   });
 });
 
