e5b97df2c2b777e836833c6a56ea4c604a2fdcad
caahcavalcantte <caahcavalcante.alfa@gmail.com>
2024-08-01 09:59:07 -0300

    config footer

--- script.js
+++ script.js
@@ -19,6 +19,68 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+// instagram
+const imagem = document.querySelector('.rede-insta img');
+
+const svgOriginal = 'images/square-instagram-brands-solid (2).svg';
+const svgHover = 'images/square-instagram-brands-solid.svg';
+
+
+imagem.addEventListener('mouseover', function(){
+    this.src = svgHover;
+});
+
+imagem.addEventListener('mouseout', function(){
+    this.src = svgOriginal;
+});
+
+// facebook
+const imagemFacebook = document.querySelector('.rede-face img');
+
+const svgOriginalFacebook = 'images/square-facebook-brands-solid (2).svg';
+const svgHoverFacebook = 'images/square-facebook-brands-solid.svg';
+
+imagemFacebook.addEventListener('mouseover', function() {
+    this.src = svgHoverFacebook;
+});
+
+imagemFacebook.addEventListener('mouseout', function() {
+    this.src = svgOriginalFacebook;
+});
+
+// whatsapp
+const imagemWhats = document.querySelector('.rede-whats img');
+
+const svgOriginalWhats = 'images/square-whatsapp-brands-solid (2).svg';
+const svgHoverWhats = 'images/square-whatsapp-brands-solid.svg';
+
+imagemWhats.addEventListener('mouseover', function() {
+    this.src = svgHoverWhats;
+});
+
+imagemWhats.addEventListener('mouseout', function() {
+    this.src = svgOriginalWhats;
+});
+
+// linkedin
+const imagemLink = document.querySelector('.rede-link img');
+
+const svgOriginalLink = 'images/linkedin-brands-solid (2).svg';
+const svgHoverLink = 'images/linkedin-brands-solid.svg';
+
+imagemLink.addEventListener('mouseover', function() {
+    this.src = svgHoverLink;
+});
+
+imagemLink.addEventListener('mouseout', function() {
+    this.src = svgOriginalLink;
+});
+
+
+
+
+
+
 
 
 
