4ecba2273921c641b50cd3c215402c7bbfe2c7ad
caahcavalcantte <caahcavalcante.alfa@gmail.com>
2024-08-02 08:35:55 -0300

    validando formulario com script

--- script.js
+++ script.js
@@ -18,6 +18,103 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+// Formulário Validação
+
+const form = document.getElementById('form');
+const campos = document.querySelectorAll('.required');
+const spans = document.querySelectorAll('.span-required');
+
+form.addEventListener('submit', (event) => {
+    let campoValid = true;
+
+    if(!nameValidate()) campoValid = false;
+    if(!pessoasValidate()) campoValid = false;;
+    if(!dataValidate()) campoValid = false;;
+    if(!horaValidate()) campoValid = false;;
+
+    if (!campoValid) {
+        event.preventDefault();
+    }
+});
+
+
+// Função validar error
+function setError(index){
+    campos[index].style.border = '2px solid #e63636';
+    spans[index].style.display = 'block';
+
+}
+
+// Função remover error
+function removeError(index){
+    campos[index].style.border = '';
+    spans[index].style.display = 'none';
+}
+
+function nameValidate(){
+    if(campos[0].value.length < 7)
+    {
+        setError(0);
+        return false;
+    }
+    else
+    {
+        removeError(0);
+        return true;
+    }
+};
+
+function pessoasValidate(){
+    const pessoas = campos[1].value;
+    if(pessoas === '0')
+        {
+            setError(1);
+            return false;
+    }
+    else
+    {
+        removeError(1);
+        return true;
+    }
+};
+
+function dataValidate(){
+    const data = campos[2].value;
+    const dataRegex =  /^\d{2}\/\d{2}\/\d{4}$/;
+
+    if(!dataRegex.test(data))
+        {
+            setError(2);
+            return false;
+    }
+    else
+    {
+        removeError(2);
+        return true;
+    }
+};
+
+function horaValidate(){
+    const hora = campos[3].value;
+
+    if(hora === '0')
+        {
+            setError(3);
+            return false;
+    }
+    else
+    {
+        removeError(3);
+        return true;
+    }
+};
+
+
+
+
+
+
+
 
 
 // instagram
